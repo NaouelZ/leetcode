2c7b6764f67a9d50e2884e87a9f2d43e6a281abd
Naouel Zenati <zenati.naouel14@gmail.com>
2022-09-11 10:21:20 +0200

    leetcode solutions for 278, 35, 704, 977

---
+++ 977_squares_of_a_sorted_array.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortedSquares = function(nums) {
+    let squares = nums.map(num => num*num)
+    return squares.sort((a, b) => a - b)
+};
